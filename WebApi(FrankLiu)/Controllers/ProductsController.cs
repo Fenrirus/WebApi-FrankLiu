1d9fd983d7ac4521e7c6d059f774c22d2cc2fd6a
Marian Marian <fenrir55@gmail.com>
2021-02-26 20:15:55 +0100

    Binding

--- WebApi(FrankLiu)/Controllers/ProductsController.cs
+++ WebApi(FrankLiu)/Controllers/ProductsController.cs
@@ -11,16 +11,35 @@ namespace WebApi_FrankLiu_.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
+        /*[HttpGet]
         public string Get()
         {
             return "Lots of produtcs";
-        }
+        }*/
 
         [HttpGet("{id}")]
-        public string Get(int id)
+        public string GetById(int id, [FromQuery] bool isActive)
         {
-            return $"Lots of produtcs: {id}";
+            return $"Lots of produtcs: {id}, status: {isActive}";
         }
+
+        [HttpGet]
+        public string GetByObject([FromQuery] ProductDTO productDTO)
+        {
+            return $"Product id {productDTO.Id} has name: {productDTO.Name}";
+        }
+
+        [HttpPost]
+        public IActionResult PostObject([FromBody] ProductDTO productDTO)
+        {
+            return Ok($"Product id {productDTO.Id} has name: {productDTO.Name}");
+        }
+    }
+
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
     }
 }
