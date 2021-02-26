0678a34b61a60d252c002f3df0718bc1de2fb1f1
Marian Marian <fenrir55@gmail.com>
2021-02-26 19:42:36 +0100

    Add project files.

---
+++ WebApi(FrankLiu)/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_FrankLiu_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Lots of produtcs";
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"Lots of produtcs: {id}";
+        }
+    }
+}
